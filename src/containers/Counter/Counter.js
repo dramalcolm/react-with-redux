c6425657f7d8552758a1d5d2d74cad280ee1b5f1
Douglas A. Malcolm <douglas@876synergy.com>
2018-08-25 12:40:39 -0500

    splitting reducers

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -21,7 +21,7 @@ class Counter extends Component {
                 <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
                 <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter}  />
                 <hr/>
-                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <button onClick={()=>this.props.onStoreResult(this.props.ctr)}>Store Result</button>
                 <ul>
                     {this.props.storeResults.map(strrs=>(
                         <li key={strrs.id} onClick={()=>this.props.onDeleteResult(strrs.id)}>{strrs.value}</li>
@@ -35,8 +35,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter,
-        storeResults: state.results
+        ctr: state.ctr.counter,
+        storeResults: state.res.results
     };
 }
 
@@ -46,7 +46,7 @@ const mapDispatchToProps = dispatch =>{
         onDecrementCounter: ()=>dispatch({type: actionTypes.DECREMENT}),
         onAddCounter: ()=>dispatch({type: actionTypes.ADD, value: 5}),
         onSubtractCounter: ()=>dispatch({type: actionTypes.SUBTRACT, value: 5}),
-        onStoreResult: ()=>dispatch({type: actionTypes.STORE_RESULT}),
+        onStoreResult: (res)=>dispatch({type: actionTypes.STORE_RESULT,result: res}),
         onDeleteResult: (id)=>dispatch({type: actionTypes.DELETE_RESULT, resultID: id}),
 
     };
